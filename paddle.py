e56ee70081d07896d438b2ea840a9867522a1ac8
noorasif05 <noorasif.noor05@gmail.com>
2025-08-25 16:20:56 +0500

    added docstrings to paddle.py

--- paddle.py
+++ paddle.py
@@ -1,7 +1,21 @@
 from turtle import Turtle
 
 class Paddle(Turtle):
+    """
+        A Paddle object for the Pong game, inherited from the Turtle class.
+
+        The paddle can move up and down along the screen.
+        It is represented as a stretched square using Turtle graphics.
+"""
     def __init__(self,position):
+        """
+            Initialize the paddle with a given position.
+
+            Parameters
+            ----------
+            position : tuple
+                The (x, y) coordinates for the paddle's initial location.
+            """
         super().__init__()
         self.shape("square")
         self.shapesize(stretch_wid=5 ,stretch_len=1, outline=1)
@@ -11,9 +25,11 @@ class Paddle(Turtle):
 
 
     def up(self):
+        """Move the paddle upward by 20 units."""
         new_y_cor = self.ycor() + 20
         self.goto(self.xcor(), new_y_cor)
 
     def down(self):
+        """Move the paddle downward by 20 units."""
         new_y_cor = self.ycor() - 20
         self.goto(self.xcor(), new_y_cor)
